60b23d704d27debe2084030bcd24e3fc58eb4784
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-03-21 13:31:23 +0300

    Add code for the home and details page

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import { MdChevronLeft } from 'react-icons/md';
+import { FaMicrophone, FaCog } from 'react-icons/fa';
+import styles from '../styles/header.module.css';
+
+const Header = () => (
+  <div className={styles.display__flex}>
+    <NavLink to="/">
+      <MdChevronLeft size={45} />
+      {' '}
+    </NavLink>
+
+    <div className={styles.header}>
+      <h1 className={styles.page__header}>Country Stats</h1>
+    </div>
+    <div className={styles.icons}>
+      <FaMicrophone size={20} />
+      <FaCog size={20} />
+    </div>
+  </div>
+);
+
+export default Header;
